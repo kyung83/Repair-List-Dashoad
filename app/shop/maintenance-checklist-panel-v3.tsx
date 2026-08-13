f449ac432cc9c8eabc4dbf69c44549399f0a5a78
kyung83 <161100189+kyung83@users.noreply.github.com>
2026-08-13 09:48:24 -0400

    Wire technician signoff into inspections

--- app/shop/maintenance-checklist-panel-v3.tsx
+++ app/shop/maintenance-checklist-panel-v3.tsx
@@ -1,2 +1,7 @@
 "use client";
-export {default} from './maintenance-checklist-panel-v2';
+import MaintenanceChecklistPanelV2 from './maintenance-checklist-panel-v2';
+import InspectionFinal from './inspection-final';
+import type {Part} from './maintenance-types';
+
+type Props={repairId:string;canWork:boolean;parts?:Part[]};
+export default function MaintenanceChecklistPanelV3(props:Props){return <><MaintenanceChecklistPanelV2 {...props}/><InspectionFinal repairId={props.repairId} canWork={props.canWork}/></>}
